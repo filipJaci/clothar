25d559c1ab97ed65cbd2263f66da1966afb0d29d
filipJaci <filip.jakovljevic@hotmail.com>
2022-04-11 10:38:52 +0200

    Used in debuging of Email Verification on production

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -2,17 +2,30 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\App;
 
-class HttpsProtocol {
+use Illuminate\Http\Request;
 
+class HttpsProtocol
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
     public function handle($request, Closure $next)
     {
-        if (!$request->secure() && App::environment() === 'production') {
-            Request::setTrustedProxies([$request->getClientIp()],Request::HEADER_X_FORWARDED_ALL);
-            return redirect()->secure($request->getRequestUri());
+        if (!app()->environment('local')) {
+            // for Proxies
+            Request::setTrustedProxies([$request->getClientIp()], 
+                Request::HEADER_X_FORWARDED_ALL);
+
+            if (!$request->isSecure()) {
+                return redirect()->secure($request->getRequestUri());
+            }
         }
 
-        return $next($request); 
+        return $next($request);
     }
 }
